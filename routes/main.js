a57873d40b970396cff13c0213b3c7c4c7202885
CleoMugs <cleophas.mugeni@gmail.com>
2020-07-09 21:44:29 +0300

    added form

--- routes/main.js
+++ routes/main.js
@@ -4,8 +4,15 @@ const router = vertex.router()
 
 router.get('/', (req, res) => {
 	const data = {
+		image_profile: 'https://pbs.twimg.com/profile_images/994716153606914048/i6UMX-ie_400x400.jpg',
 		greeting: 'Hello! Welcome to my portfolio site!',
-		introduction: 'I am a web developer from Kenya'
+		introduction: 'I am a web developer from Kenya',
+		languages: [
+			{name:'javascript', years:5},
+			{name:'python', years:4},
+			{name:'swift', years:5},
+			{name:'c#', years:1}
+		]
 	}
 	res.render('landing', data)
 
