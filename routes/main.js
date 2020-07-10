ddf85c20c304eef8381da8146865b2c95ccaecd5
CleoMugs <cleophas.mugeni@gmail.com>
2020-07-10 10:12:04 +0300

    designed navbar

--- routes/main.js
+++ routes/main.js
@@ -3,17 +3,8 @@ const router = vertex.router()
 
 
 router.get('/', (req, res) => {
-	const data = {
-		image_profile: 'https://pbs.twimg.com/profile_images/994716153606914048/i6UMX-ie_400x400.jpg',
-		greeting: 'Hello! Welcome to my portfolio site!',
-		introduction: 'I am a web developer from Kenya',
-		languages: [
-			{name:'javascript', years:5},
-			{name:'python', years:4},
-			{name:'swift', years:5},
-			{name:'c#', years:1}
-		]
-	}
+
+	const data = req.context // {page:..., global:...}
 	res.render('landing', data)
 
 })
