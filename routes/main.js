839070d5517f108f526fd6bc22fec503e5c87f1b
CleoMugs <cleophas.mugeni@gmail.com>
2020-07-10 17:21:29 +0300

    created db

--- routes/main.js
+++ routes/main.js
@@ -4,17 +4,18 @@ const ProjectController = require('../controllers/ProjectController')
 
 
 router.get('/', (req, res) => {
+	const data = req.context // {page:..., global:...}
+
 	const projectCtr = new ProjectController()
 	projectCtr.get()
 	.then(projects => {
-
+		data['projects'] = projects
+		//console.log('Projects:' + JSON.stringify(projects))
+		res.render('landing', data)
 	})
 	.catch(err => {
-		res.send('Oops!' '+err.message')
+		res.send('Oops! '+err.message)
 	})
-
-	const data = req.context // {page:..., global:...}
-	res.render('landing', data)
 
 })
 
